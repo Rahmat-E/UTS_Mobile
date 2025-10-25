87d3c66a8ab2d47ecae153a8964959aef91faf7e
Rahmat-E <230605110010@student.uin-malang.ac.id>
2025-10-25 19:47:58 +0700

    AnimeRepository.dart
    Added comments to explain the purpose of the AnimeRepository class and its load method.

--- lib/services/anime_repository.dart
+++ lib/services/anime_repository.dart
@@ -1,7 +1,10 @@
+// mengonversi data dari file JSON lokal (assets/anime.json)
+// menjadi daftar objek Anime agar bisa digunakan di seluruh aplikasi.
 import 'dart:convert';
 import 'package:flutter/services.dart' show rootBundle;
 import '../models/anime.dart';
-
+// Kelas AnimeRepository digunakan untuk mengambil data anime
+// dari file lokal menggunakan rootBundle (fungsi bawaan Flutter)
 class AnimeRepository {
   static Future<List<Anime>> load() async {
     final raw = await rootBundle.loadString('assets/anime.json');
@@ -9,3 +12,4 @@ class AnimeRepository {
     return data.map((e) => Anime.fromJson(e as Map<String, dynamic>)).toList();
   }
 }
+
